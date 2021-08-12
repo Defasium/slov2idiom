69a0e512d2f5608f9b2b15bd71efe88d18f8f543
Ishkov Denis <47502256+Defasium@users.noreply.github.com>
2021-08-12 12:02:00 +0200

    echo_bot_test

---
+++ bot.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This program is dedicated to the public domain under the CC0 license.
+
+"""
+Simple Bot to reply to Telegram messages.
+
+First, a few handler functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+
+Usage:
+Basic Echobot example, repeats messages.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
+import os
+import telebot
+from flask import Flask, request
+
+TOKEN = os.environ.get('TG_TOKEN', '')
+APP_URL = os.path.join(os.environ.get('APP_URL', ''), TOKEN)
+bot = telebot.TeleBot(TOKEN)
+server = Flask(__name__)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, ''.join(['Hello, ', message.from_user.first_name]))
+
+
+@bot.message_handler(func=lambda _: True, content_type=['text'])
+def echo(message):
+    bot.reply_to(message, message.text)
+
+
+@server.route('/', TOKEN, methods=['POST'])
+def get_message():
+	json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return '!', 200
+
+
+@server.route('/')
+def webhook():
+    bot.remove_webhook()
+    bot.send_webhook(url=APP_URL)
+    return '!', 200
+
+
+if __name__=='__main__':
+    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
