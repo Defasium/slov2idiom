ec4d89f5462bcfdd364cc9bc34137b8b7f9661bf
Ishkov Denis <47502256+Defasium@users.noreply.github.com>
2021-08-12 12:06:12 +0200

    tab fixes

--- bot.py
+++ bot.py
@@ -36,7 +36,7 @@ def echo(message):
 
 @server.route('/', TOKEN, methods=['POST'])
 def get_message():
-	json_string = request.get_data().decode('utf-8')
+    json_string = request.get_data().decode('utf-8')
     update = telebot.types.Update.de_json(json_string)
     bot.process_new_updates([update])
     return '!', 200
