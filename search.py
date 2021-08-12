ecd0f0f8e38a01b70dcb9fd56dd8d5a2a82dc855
Ishkov Denis <47502256+Defasium@users.noreply.github.com>
2021-08-12 17:22:22 +0200

    Add files via upload

--- search.py
+++ search.py
@@ -36,7 +36,7 @@ def search_idiom(prompt, num=10):
 def construct_table(rows):
     result = []
     for i, row in enumerate(rows):
-        result.append('%d. *%s* | %s\n'%(i+1, *row))
+        result.append('%d. *%s* | %s\n'%(i+1, row[0].upper(), row[1]))
     return ''.join(result)
 
 
